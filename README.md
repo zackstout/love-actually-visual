86bf46427dce3b97707fb01da21e369929b46a8c
Zack Stout <zackstout@gmail.com>
2018-02-02 23:56:49 -0600

    well it's basically accomplishing what i wanted it to do. needs a bit more to feel really representative, though

---
+++ README.md
@@ -0,0 +1,3 @@
+# Love Actually Visualization
+
+The aim is to visualize the connections among all the key players in *Love Actually*, using FiveThirtyEight's dataset and vanilla HTML canvas.
--- client.js
+++ client.js
@@ -10,9 +10,7 @@ var ctx, canvas;
       console.log(canvas);
       ctx = canvas.getContext('2d');
 
-      polygon(1, 1, 14, 200);
-
-
+      polygon(1, 1, 14, 260);
 
     }
 
@@ -48,19 +46,50 @@ var connections = [
   [1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 2]
 ];
 
+// Ok if i is 2, we go and draw line between 2 and 0 (width 2) and 1 (width 7)
+
+// In our case, x is the number of actors, 14:
 function polygon(a, b, x, r) {
   ctx.translate(canvas.width/2, canvas.height/2);
-  ctx.beginPath();
   for (var i = 0; i < x; i++) {
     // I suppose we can ignore the values on the diagonal, because those just count how many scenes each actor appears in. Or we could use that to size their circle...
-
-    
-    ctx.moveTo(r*a*Math.cos(i*2*Math.PI/x), r*b*Math.sin(i*2*Math.PI/x));
-    ctx.lineTo(r*a*Math.cos((i+1)*2*Math.PI/x), r*b*Math.sin((i+1)*2*Math.PI/x));
+    var xCoord = r * a * Math.cos(i * 2 * Math.PI/x);
+    var yCoord = r * b * Math.sin(i * 2 * Math.PI/x);
+    var row = connections[i];
+    ctx.beginPath();
+    // Move to the ith row in our matrix:
+    ctx.moveTo(xCoord, yCoord);
+    // Draw circle to represent amount of scenes appeared in (soon to be filled with actor's face):
+    ctx.arc(xCoord, yCoord, 4 * row[row.length - 1], 0, 2 * Math.PI);
+    ctx.fillStyle = getRandomColor();
+    ctx.fill();
+    // ctx.lineTo(r*a*Math.cos((i+1)*2*Math.PI/x), r*b*Math.sin((i+1)*2*Math.PI/x));
     ctx.stroke();
+
+    for (var j=0; j < row.length - 1; j++) {
+      // Honestly unsure whether either are needed:
+      ctx.beginPath();
+      ctx.moveTo(xCoord, yCoord);
+      var targetDiff = i - j;
+      var targetX = r * a * Math.cos((i - targetDiff) * 2 * Math.PI/x);
+      var targetY = r * b * Math.sin((i - targetDiff) * 2 * Math.PI/x);
+      ctx.lineTo(targetX, targetY);
+      ctx.lineWidth = row[j] * 3;
+      ctx.stroke();
+    }
+
   }
   ctx.translate(-canvas.width/2, -canvas.height/2);
 
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 
